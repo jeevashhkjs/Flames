4653ed4e992674ac03496d4c7ef4597ea8174113
jeevashhkjs <jeevasdckap@gmail.com>
2023-03-06 12:39:53 +0530

    My first push no code

---
+++ jasmine.js
@@ -0,0 +1,11 @@
+function flames(name1,name2){
+    let nm1 = name1.split("")
+    let nm2 = name2.split("")
+
+    let nm1l = name1.length
+    let nm2l = name2.length
+
+    let len = nm1l + nm2l
+
+}
+flames("jeeva","jasmine")
